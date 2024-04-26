ddf46fc24dc27fe9f3f83a7f5faa9a77899b24f4
afbstorm <aurelien.freyling@bstorm.be>
2024-04-26 11:59:55 +0200

    Update validators

--- validators/register.validator.js
+++ validators/register.validator.js
@@ -2,9 +2,6 @@ const yup = require('yup');
 const { object } = require('yup');
 
 const registerValidator = object({
-    nom: yup.string().min(2).max(50).required(),
-    prenom: yup.string().min(2).max(50).required(),
-    age: yup.number(),
     password: yup.string().min(1).max(50).required(),
     email: yup.string().min(5).max(100).required(),
 
